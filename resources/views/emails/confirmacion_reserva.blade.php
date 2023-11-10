8366e46a430f52d44485f8f4ec523074c18f9722
JSebastianDP <sebastiandiaz28001@gmail.com>
2023-11-09 21:16:22 -0500

    Se crea la vista con el contenido del mensaje, queda pendiente configurar el correo para el envio

---
+++ resources/views/emails/confirmacion_reserva.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Confirmación de Reserva</title>
+</head>
+<body>
+    <h1>Confirmación de Reserva</h1>
+    <p>Hola {{ $reserva->cliente_email }},</p>
+    <p>Tu reserva ha sido confirmada. Gracias por elegir nuestro servicio.</p>
+</body>
+</html>
